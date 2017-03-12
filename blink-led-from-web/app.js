9a014669b911e8fec743975ca89f70e2d08c0bd5
Ionut Pistol <ionut.pistol@gmail.com>
2017-03-12 21:34:33 +0200

    Instantiate arduino board and manage LED state

--- blink-led-from-web/app.js
+++ blink-led-from-web/app.js
@@ -9,6 +9,13 @@ const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/'));
 
+var board = new five.Board();
+var led = 3;
+
+board.on('ready', function() {
+    led = five.Led(11);
+});
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -19,9 +26,32 @@ io.on('connection', function(client) {
     });
 
     client.on('state', function(data) {
-        console.log('I received: ' + data.state);
+        console.log(data.state);
+        if (data && led) {
+            switch(data.state) {
+                case 'on':
+                    led.on();
+                    break;
+                case 'pulse':
+                    led.pulse(300);
+                    break;
+                case 'strobe':
+                    led.strobe(600);
+                    break;
+                case 'fade':
+                    led.fadeIn();
+                    setTimeout(function() {
+                        led.fadeOut();
+                    }, 2000);
+                    break;
+                default: 
+                    led.stop().off();
+            }
+        }
     });
 });
+
+console.log(process.env.PORT);
 
 const port = process.env.PORT || 5000;
 server.listen(port);
--- blink-led-from-web/client.js
+++ blink-led-from-web/client.js
@@ -2,7 +2,6 @@
     let socket = io.connect(window.location.hostname + ':' + 5000);
 
     function emitState(event) {
-        console.log('event: ', event.target.id);
         socket.emit('state', {  
                 state: event.target.id 
             }
@@ -13,11 +12,13 @@
     let pulseButton = document.getElementById('pulse');
     let strobeButton = document.getElementById('strobe');
     let offButton = document.getElementById('off');
+    let fadeButton = document.getElementById('fade');
 
     onButton.addEventListener('click', emitState, false);
     pulseButton.addEventListener('click', emitState, false);
     strobeButton.addEventListener('click', emitState, false);
     offButton.addEventListener('click', emitState, false);
+    fadeButton.addEventListener('click', emitState, false);
 
     socket.on('connect', function(data) {
         socket.emit('join', 'Client is connected');
